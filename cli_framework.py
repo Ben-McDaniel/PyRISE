8db6749c57daa2d9ec1450434360d027e4ecabca
Ben McDaniel <47250557+Ben-McDaniel@users.noreply.github.com>
2023-03-31 19:18:01 -0400

    changed structure and removed click

--- cli_framework.py
+++ cli_framework.py
@@ -1,5 +1,26 @@
-import click
+# import click
 
-@click.command()
-def cli-framework():
-    print("Hello World!")
+# @click.command()
+# def cli-framework():
+#     print("Hello World!")
+
+import argparse
+
+class CLI:
+    def __init__(self, prog_name):
+        self.parser = argparse.ArgumentParser(prog=prog_name)
+        self.args = None
+
+    def add_argument(self, *args, **kwargs):
+        self.parser.add_argument(*args, **kwargs)
+
+    def run(self):
+        self.args = self.parser.parse_args()
+        # Add your unique code here
+
+
+if __name__ == '__main__':
+    cli = CLI(prog_name='my_program')
+    cli.add_argument('--foo', help='foo help message')
+    cli.add_argument('--bar', help='bar help message')
+    cli.run()
